bda49ccf4c9230220d389cf02e642635eb65a1a6
rafmanrique <rafamanrique26@gmail.com>
2022-04-29 19:03:51 +0000

    Diagram Done

--- src/models.py
+++ src/models.py
@@ -8,26 +8,45 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    password = Column(String(15), nullable=False)
+    first_name = Column(String(15), nullable=False)
+    last_name = Column(String(15), nullable=False)
+    email = Column(String(50), nullable=False, unique = True)
+    favorites = relationship('favorites')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    user = relationship('user')
+    user = relationship('planet')
 
-    def to_dict(self):
-        return {}
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    population = Column(Integer, nullable=False)
+    favorites = relationship('favorites')
+    
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    age = Column(Integer)
+
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(50), nullable=False)
+    vehicle_type = Column(String(50), nullable=False)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
